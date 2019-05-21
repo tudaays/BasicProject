3b596ed4d2d6d378297771bfa7d3c31cd92a14c7
tudaays <tuda.ays@gmail.com>
2019-05-21 22:44:58 +0700

    Give back the terminal state to user

--- TextEditor/TextEditor.c
+++ TextEditor/TextEditor.c
@@ -1,11 +1,18 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode(){
-    struct termios raw;
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
 
-    tcgetattr(STDIN_FILENO, &raw);
-
+    struct termios raw = orig_termios;
     raw.c_lflag &= ~(ECHO);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
@@ -13,7 +20,7 @@ void enableRawMode(){
 
 int main(){
     enableRawMode();
-    
+
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
